0962fe1d585309a332a11ac8811e6712b3aac5ce
LUIZDV1D <gamesbrdavi@gmail.com>
2019-08-21 15:29:06 -0300

    site_money

---
+++ precache-manifest.66012cd4dc90292b0e82b8dfa0b4d8ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd759109a919e3424f7ad4336ec98fba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfa63f598b22df4095a",
+    "url": "/static/css/main.34550434.chunk.css"
+  },
+  {
+    "revision": "a360d3ecbf28f924c3f0",
+    "url": "/static/js/2.c5228ffa.chunk.js"
+  },
+  {
+    "revision": "cdfa63f598b22df4095a",
+    "url": "/static/js/main.08a792a6.chunk.js"
+  },
+  {
+    "revision": "590112dd0e163c5a6401",
+    "url": "/static/js/runtime~main.c21db47a.js"
+  }
+]);
